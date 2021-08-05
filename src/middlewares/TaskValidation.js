ebdcc71dc00b4bfa232adb71f24932b83714c763
Dener Dev <dener3d.designer@gmail.com>
2021-08-04 22:39:33 -0300

    Update TaskValidation.js

--- src/middlewares/TaskValidation.js
+++ src/middlewares/TaskValidation.js
@@ -1,14 +1,14 @@
 const TaskModel = require('../model/TaskModel');
 
 const TaskValidation = async (req, res, next) => {
-    const { mac, titulo, descricao, data_fim } = req.body;
+    const { mac, title, description, due_on} = req.body;
     if (!mac)
     return res.status(400).json({ error: 'O mac é obrigatório!'})
-    else if (!titulo)
+    else if (!title)
     return res.status(400).json({ error: "O título é obrigatório!" })
-    else if (!descricao)
+    else if (!description)
     return res.status(400).json({error: "A descrição é obrigatória!"})
-    else if (!data_fim)
+    else if (!due_on)
     return res.status(400).json({error: "A data final da tarefa é obrigatória!"})
     else
     next()
